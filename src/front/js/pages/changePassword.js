1e0f010966713f367a177588b89e61935b5cf6be
jhntnc <JHONATANCALDERON2104@GMAIL.COM>
2023-01-08 04:02:47 +0000

    page changePassword

--- src/front/js/pages/changePassword.js
+++ src/front/js/pages/changePassword.js
@@ -6,9 +6,36 @@ export const ChangePassword = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>ChangePassword</h1>
-			
-		</div>
+
+<div className="container">
+	<div className="card text-center">
+
+  <div className="card-header fs-1">
+    Cambio de Contraseña
+  </div>
+
+  	<div className="card-body">
+		<div className="container">
+             <div className="form-floating mb-2 col-12 col-md-6 offset-md-3">
+             <input type="password" className="form-control" id="floatingPassword" placeholder="Password"></input>
+             <label for="floatingPassword">Contraseña Actual</label>
+              </div>
+
+            <div className="form-floating mb-2 col-12 col-md-6 offset-md-3">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"></input>
+            <label for="floatingPassword">Nueva Contraseña</label>
+            </div>
+
+            <div className="form-floating mb-2 col-12 col-md-6 offset-md-3">
+  			<input type="password" class="form-control" id="floatingPassword" placeholder="Password"></input>
+  			<label for="floatingPassword">Repetir Contraseña</label>
+			</div>
+   
+    		<a href="#" className="btn btn-dark col-5">Cambiar</a>
+			</div>
+  	</div>
+</div>
+</div>
+
 	);
 };
