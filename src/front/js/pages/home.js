4c40ada993c05023a940e4ba932b8329b24d1adb
Gabriel-Garrido <gabrielgarrido89@gmail.com>
2023-01-09 02:04:37 +0000

    corrección en diseño e idioma de DOM en componente Home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -12,11 +12,11 @@ export const Home = () => {
 
 			<div className="card text-center">
 				<div className="card-header">
-					<p>Exchange rate</p>
+					<p className="fs-1">Tasa de Cambio</p>
 					
 						<div className="dropdown mb-3">
-							<button className="btn btn-outline-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-							Select exchange
+							<button className="btn btn-outline-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Seleccione divisa
 							</button>
 							<ul className="dropdown-menu">
 								<li><a className="dropdown-item" href="#">1 CLP / 890 USD</a></li>
@@ -31,18 +31,18 @@ export const Home = () => {
 					<div className="mb-3 d-flex flex-column align-items-center col-8 offset-2 col-md-4 offset-md-4 ">
 						
 						<div className="input-group">
-							<input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" placeholder="Enter the amount to change"></input>
+							<input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" placeholder="Ingrese el monto a enviar"></input>
 							<span className="input-group-text" id="basic-addon3">CLP</span>
 						</div>
-						<div className="form-text fs-5">you will change:</div>
+						<div className="form-text fs-5">Usted va a cambiar:</div>
 						<p className="fs-1">x CLP to x USD</p>
 					
 						
 						{/* Selección de cuenta bancaria */}
 							<div classList="container">
 					
-								<button class="btn btn-outline-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-									Select bank account  <i class="fas fa-caret-down"></i>
+								<button class="btn btn-outline-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+									Seleccione cuenta bancaria  <i class="fas fa-caret-down"></i>
 								</button>
 								
 								<div class="collapse" id="collapseExample">
@@ -51,16 +51,16 @@ export const Home = () => {
 										<ul class="list-group">
 						
 											<input type="radio" class="btn-check" name="options" id="option1" autocomplete="off"></input>
-											<label class="btn btn-outline-primary" for="option1">Bank account 1</label>
+											<label class="btn btn-outline-dark" for="option1">Cuenta bancaria 1</label>
 
 											<input type="radio" class="btn-check" name="options" id="option2" autocomplete="off"></input>
-											<label class="btn btn-outline-primary" for="option2">Bank account 2</label>
+											<label class="btn btn-outline-dark" for="option2">Cuenta bancaria 2</label>
 
 											<input type="radio" class="btn-check" name="options" id="option3" autocomplete="off"></input>
-											<label class="btn btn-outline-primary" for="option3">Bank account 3</label>
+											<label class="btn btn-outline-dark" for="option3">Cuenta bancaria 3</label>
 
 											<input type="radio" class="btn-check" name="options" id="option4" autocomplete="off"></input>
-											<label class="btn btn-outline-primary" for="option4">Bank account 4</label>
+											<label class="btn btn-outline-dark" for="option4">Cuenta bancaria 4</label>
 										
 										</ul>
 									</div>
@@ -73,7 +73,7 @@ export const Home = () => {
 						{/* /Selección de cuenta bancaria */}
 
 
-						<Link to="/process" className="btn btn-primary col-8 offset-2 col-md-4 offset-md-4">Process change</Link>
+						<Link to="/process" className="btn btn-dark col-8 offset-2 col-md-4 offset-md-4 fs-4">Procesar cambio</Link>
 				</div>
 			</div>
 			
