afd0f221192ca878d583c031b1c3c300b2108d02
Gabriel-Garrido <gabrielgarrido89@gmail.com>
2023-01-09 02:35:43 +0000

    corrección diseño DOM en componente NewBankAccount

--- src/front/js/pages/newBankAccount.js
+++ src/front/js/pages/newBankAccount.js
@@ -6,13 +6,15 @@ export const NewBankAccount = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
+
+		<div className="container">
 		
 <div className="card text-center row">
                  <div className="card-header fs-1">
                     Nueva Cuenta Bancaria
                 </div>
   <div className="card-body d-flex flex-column aling-items-center ">
-
+  <div className="container">
 		 <div>
            <label for="name">Nombre de Banco</label>
         </div>
@@ -57,16 +59,16 @@ export const NewBankAccount = () => {
 		  <input type="text" id="name" name="name" required
        minlength="4" maxlength="17" size="35"/>
 		  </div>
+			</div>
 
-
-        <a href="#" className="btn btn-primary col-2 offset-5 mt-4">Guardar</a>
+        <a href="#" className="btn btn-dark col-8 offset-2 col-md-2 offset-md-5 mt-4">Guardar</a>
     
   </div>
   
   <div className="card-footer text-muted">
-    2 days ago
   </div>
   
+</div>
 </div>
 
 		
