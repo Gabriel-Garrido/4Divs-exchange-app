405d5ea4288cfae46259570de9b3888b5a76ea6a
Gabriel-Garrido <gabrielgarrido89@gmail.com>
2023-01-12 03:16:06 +0000

    agrego endpoints de users

--- src/api/models.py
+++ src/api/models.py
@@ -9,17 +9,42 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     validate_status = db.Column(db.Boolean(), unique=False, nullable=False)
-    first_name = db.Column(db.String(30), unique=True, nullable=False)
-    last_name = db.Column(db.String(30), unique=True, nullable=False)
+    first_name = db.Column(db.String(30), unique=False, nullable=False)
+    last_name = db.Column(db.String(30), unique=False, nullable=False)
     phone = db.Column(db.String(13), unique=True, nullable=False)
-    birthdate = db.Column(db.String(13), unique=True, nullable=False)
-    nationality = db.Column(db.String(30), unique=True, nullable=False)
-    ocupation = db.Column(db.String(30), unique=True, nullable=False)
-    monthly_income = db.Column(db.Integer, unique=True, nullable=False)
-    particular_address = db.Column(db.String(120), unique=True, nullable=False)
-    department = db.Column(db.String(120), unique=True, nullable=False)
+    birthdate = db.Column(db.String(50), unique=False, nullable=False)
+    nationality = db.Column(db.String(30), unique=False, nullable=False)
+    ocupation = db.Column(db.String(30), unique=False, nullable=False)
+    monthly_income = db.Column(db.Integer, unique=False, nullable=False)
+    particular_address = db.Column(db.String(120), unique=False, nullable=False)
+    department = db.Column(db.String(120), unique=False, nullable=False)
 
-    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class Change(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    origin_exchange = db.Column(db.String(30), unique=False, nullable=False)
+    destination_exchange = db.Column(db.String(30), unique=False, nullable=False)
+    exchange_rate = db.Column(db.String(30), unique=False, nullable=False)
+
+class Bank_account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    country = db.Column(db.String(40), unique=False, nullable=False)
+    account_number = db.Column(db.String(40), unique=False, nullable=False)
+    bank = db.Column(db.String(40), unique=False, nullable=False)
+    account_holder = db.Column(db.String(100), unique=False, nullable=False)
+    document_type = db.Column(db.String(40), unique=False, nullable=False)
+    document_id = db.Column(db.String(100), unique=True, nullable=False)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    status = db.Column(db.String(11), unique=False, nullable=False)
+    change_id = db.Column(db.Integer, db.ForeignKey('change.id'), nullable=False)
+    bank_account_id = db.Column(db.Integer, db.ForeignKey('bank_account.id'), nullable=False)
+    date = db.Column(db.String(50), unique=False, nullable=False)
+    time = db.Column(db.String(50), unique=False, nullable=False)
+    transaction_amount = db.Column(db.String(50), unique=False, nullable=False)
+    transfer_bank_id = db.Column(db.String(50), unique=True, nullable=False)
 
     def __repr__(self):
         return f'<User {self.user_id}>'
@@ -39,6 +64,8 @@ class User(db.Model):
             "monthly_income": self.monthly_income,
             "particular_address": self.particular_address,
             "department": self.department,
+
+
 
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -8,28 +8,118 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 user_temp = [{
+    "user_id": "0",
+    "rut": "1234567-5",
+    "email": "pepito@gmail.com",
+    "password": "pepe123",
+    "validate_status": "True",
+    "first_name": "Pepito",
+    "last_name": "Perez",
+    "phone": "987654321",
+    "birthdate": "01-02-1983",
+    "nationality": "chilena",
+    "ocupation": "arquitecto",
+    "monthly_income": "2000000",
+    "particular_address": "los arboles 1234",
+    "department": "1020",
+    },
+    {
     "user_id": "1",
-    "rut": "jose",
-    "email": "jose@gmail.com"
+    "rut": "1234567-5",
+    "email": "pepito@gmail.com",
+    "password": "pepe123",
+    "validate_status": "True",
+    "first_name": "Pepito",
+    "last_name": "Perez",
+    "phone": "987654321",
+    "birthdate": "01-02-1983",
+    "nationality": "chilena",
+    "ocupation": "arquitecto",
+    "monthly_income": "2000000",
+    "particular_address": "los arboles 1234",
+    "department": "1020",
+    },
+    {
+    "user_id": "2",
+    "rut": "1234567-5",
+    "email": "pepito@gmail.com",
+    "password": "pepe123",
+    "validate_status": "True",
+    "first_name": "Pepito",
+    "last_name": "Perez",
+    "phone": "987654321",
+    "birthdate": "01-02-1983",
+    "nationality": "chilena",
+    "ocupation": "arquitecto",
+    "monthly_income": "2000000",
+    "particular_address": "los arboles 1234",
+    "department": "1020",
+}
+]
+
+change_temp = [{
+    "change_id": "1",
+    "origin_exchange": "CLP",
+    "destination_exchange": "USD",
+    "exchange_rate": "890",
 }]
 
+bank_account_temp = [{
+    "bank_account_id": "1",
+    "user": "1234567-5",
+    "country": "USA",
+    "account_number": "123456789",
+    "bank": "City Bank",
+    "account_holder": "Pepito Perez",
+    "document_type": "passaport",
+    "document_id": "817916",
+    "": "",
+    }]
 
+transaction_temp = [{
+    "transaction_id": "1",
+    "user": "1234567-5",
+    "status": "True",
+    "change_id": "1",
+    "bank_account_id": "1",
+    "date": "28-12-2022",
+    "time": "14:00",
+    "transaction_amount": "500000",
+    "transfer_bank_id": "6475830485768",
+    }]
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/get_all_users/', methods=['GET'])
+def get_all_users():
 
-    response_body = {
-        "user": "pepito"
-    }
+    return jsonify(user_temp), 200
 
-    return jsonify(response_body), 200
-
-@api.route('/user/<int:user_id>', methods=['GET', 'POST'])
+@api.route('/get_user/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    if request.method == "GET":
-        return jsonify({"response": "call GET request"})
 
-    elif request.method == "POST":
-        req_Json = request.get_json()
-        user_temp.append(req_Json)
-        return jsonify(user_temp), 200
+    return jsonify(user_temp[user_id]), 200
+
+@api.route('add_user/<int:user_id>', methods=['POST'])
+def add_user(user_id):
+    req_Json = request.get_json()
+    req_Json["user_id"] = str(user_id)
+    user_temp.append(req_Json)
+    return jsonify(user_temp), 200
+
+@api.route('edit_user/<int:user_id>', methods=['PUT'])
+def edit_user(user_id):
+    req_Json = request.get_json()
+    user_temp[user_id] = req_Json
+    return jsonify(user_temp), 200
+
+@api.route('delete_user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    req_Json = request.get_json()
+    del user_temp[user_id]
+    return jsonify(user_temp), 200
+
+
+@api.route('add_bank_account/<int:bank_account_id>', methods=['POST'])
+def add_bank_account(bank_account_id):
+    req_Json = request.get_json()
+    bank_account_temp.append(req_Json)
+    return jsonify(bank_account_temp), 200
