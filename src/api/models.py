6694804b7093874b55ede14b8d8baeb1283c6d07
Gabriel-Garrido <gabrielgarrido89@gmail.com>
2023-01-13 22:44:36 +0000

    agrego __tablename__

--- src/api/models.py
+++ src/api/models.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     rut = db.Column(db.String(11), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -20,12 +21,14 @@ class User(db.Model):
     department = db.Column(db.String(120), unique=False, nullable=False)
 
 class Change(db.Model):
+    __tablename__ = 'change'
     id = db.Column(db.Integer, primary_key=True)
     origin_exchange = db.Column(db.String(30), unique=False, nullable=False)
     destination_exchange = db.Column(db.String(30), unique=False, nullable=False)
     exchange_rate = db.Column(db.String(30), unique=False, nullable=False)
 
 class Bank_account(db.Model):
+    __tablename__ = 'bank_account'
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     country = db.Column(db.String(40), unique=False, nullable=False)
@@ -36,6 +39,7 @@ class Bank_account(db.Model):
     document_id = db.Column(db.String(100), unique=True, nullable=False)
 
 class Transaction(db.Model):
+    __tablename__ = 'transaction'
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     status = db.Column(db.String(11), unique=False, nullable=False)
@@ -45,26 +49,3 @@ class Transaction(db.Model):
     time = db.Column(db.String(50), unique=False, nullable=False)
     transaction_amount = db.Column(db.String(50), unique=False, nullable=False)
     transfer_bank_id = db.Column(db.String(50), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.user_id}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "rut": self.rut,
-            "email": self.email,
-            "validate_status": self.validate_status,
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "phone": self.phone,
-            "birthdate": self.birthdate,
-            "nationality": self.nationality,
-            "ocupation": self.ocupation,
-            "monthly_income": self.monthly_income,
-            "particular_address": self.particular_address,
-            "department": self.department,
-
-
-            # do not serialize the password, its a security breach
-        }
