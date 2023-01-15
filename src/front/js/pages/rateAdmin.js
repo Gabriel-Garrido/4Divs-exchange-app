fbacab6ff1d3e3dec1deb967475a2f80e2008945
jhntnc <JHONATANCALDERON2104@GMAIL.COM>
2023-01-15 23:19:45 +0000

    cambios en el h2

--- src/front/js/pages/rateAdmin.js
+++ src/front/js/pages/rateAdmin.js
@@ -1,15 +1,19 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import Dist from "webpack-merge";
 
 export const RateAdmin = () => {
 	const { store, actions } = useContext(Context);
 
+
 	const [rate, setRate] = useState("");
+	
 
 	const handleChange = e => {
 		setRate(e.target.value);
 	  };
+
 	
 
 	return (
@@ -19,24 +23,25 @@ export const RateAdmin = () => {
 		<div className="card text-center">
 			<h1>Cambio de Tasa</h1>
 		<div className="card-header">
-		<h2>tasa actual</h2>
+		<h2>Tasa Actual</h2>
 		
 		<div className="dropdown mb-3">
 		<button className="btn btn-outline-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
 		Select exchange
 		</button>
 		<ul className="dropdown-menu">
-		<li><a className="dropdown-item" href="#">1 CLP / 890 USD</a></li>
+		<li><a className="dropdown-item" href="#">1 USD / 890 CLP</a></li>
 		<li><a className="dropdown-item text-secondary" href="#">...future options</a></li>
 		<li><a className="dropdown-item text-secondary" href="#">...future options</a></li>
 		</ul>
 		</div>
 		
-		<p className="fs-1">1 CLP / {rate} USD</p>
+		<p className="fs-1">1 USD / {rate} CLP</p>
 		</div>
 		<div className="card-body row">
 		<div className="mb-3 d-flex flex-column align-items-center col-8 offset-2 col-md-4 offset-md-4 ">
-		<label className="fs-1">1 CLP to</label>
+		<h2>Tasa Nueva</h2>
+		<label className="fs-1">1 USD to</label>
 		<input
               type="number"
               className="form-control"
@@ -45,6 +50,7 @@ export const RateAdmin = () => {
               placeholder="Nuevo Precio"
               onChange={handleChange}
             ></input>
+
 		</div>
 		<button className="btn btn-dark col-4 offset-4">
   			Cambiar
